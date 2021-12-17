b9b2d90e54df661ce0f6897e2a43ba78862f1fdf
Raspeball <Raspeball@users.noreply.github.com>
2021-12-17 19:59:16 +0100

    Mostly working. Calculate score is left to do

--- 2021/day4/day4-1.py
+++ 2021/day4/day4-1.py
@@ -1,5 +1,6 @@
 # importing
 import math
+import numpy as np
 #
 #
 #
@@ -32,3 +33,62 @@ bingo_boards = []
 for i in range(0, len(raw_boards), 5):
     curr_board = raw_boards[i:i+5]
     bingo_boards.append(curr_board)
+
+
+### --- --- ###
+
+def AddWinnerCounter(board_arr):
+
+    for board in board_arr:
+        for row in board:
+            row.append(0)
+
+        board.append([0,0,0,0,0])
+
+#AddWinnerCounter(bingo_boards)
+
+#print(bingo_boards)
+
+def CheckBoard(board_arr, b): #works
+    # b is a number, ie. a drawn bingo number
+
+    row_win = False
+    col_win = False
+    winner_board = -1
+
+    for board in board_arr:
+        for row in board:
+            if b in row:
+                row[-1] -= 1
+                board[-1][row.index(b)] -= 1
+                if row[-1] == -5:
+                    row_win = True
+                    winner_board = board_arr.index(board)
+
+                elif board[-1][row.index(b)] == -5:
+                    col_win = True
+                    winner_board = board_arr.index(board)
+
+
+    return board_arr, winner_board
+
+
+def PlayBingo(list_of_boards, nums):
+    # list_of_boards is an array of boards, which is a list of rows
+    # nums is a list of numbers to be drawn
+
+    # initialize
+
+    for n in nums:
+        update_boards, win = CheckBoard(list_of_boards, n)
+        if win != -1:
+            break
+
+
+    winner_board = list_of_boards[win]
+
+    return winner_board
+
+
+def CalculateScore(wboard):
+    
