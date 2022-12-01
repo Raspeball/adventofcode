98f1982d79bf8c33f3cb51de76a9795a48eba929
Raspeball <Raspeball@users.noreply.github.com>
2022-12-01 22:46:16 +0100

    updates

--- 2022/day1/day_1.py
+++ 2022/day1/day_1.py
@@ -1,7 +1,13 @@
-with open('input.txt') as calories:
-    elf_calories = calories.readlines() # list of string elements
+import itertools
 
-# list massage
-elf_calories_grouped = "".join(elf_calories)
+with open("day1_input.txt") as calories:
+    elf_calories = [line for line in calories]
+
+# grouping
+cal_groups = itertools.groupby(elf_calories, lambda sep: sep == "\n")
+
+elf_calories_grouped = [list(gr) for key, gr in cal_groups if not key]
+
+
 
 print(elf_calories_grouped)
