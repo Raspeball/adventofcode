623a4b78b5874f2c9c4605d1ff1e509a831f4c77
Raspeball <Raspeball@users.noreply.github.com>
2022-12-04 00:28:14 +0100

    day3 pt 2 complete

--- 2022/day3.py
+++ 2022/day3.py
@@ -6,6 +6,8 @@
 with open(r"C:\Users\Lars Rikard\Documents\GitHub\adventofcode\2022\day3-input.txt") as data:
     rucksacks = [line.strip("\n") for line in data]
 
+### --- Part one --- ###
+
 # define priorities
 lower_alph = "abcdefghijklmnopqrstuvwxyz"
 priority_alph = lower_alph + lower_alph.upper()
@@ -19,22 +21,55 @@ def CommonItemRucksack(content):
         if i in comps[1]:
             return i
 
-# sum priorities
+# calculate priorities
 def CalcPriority(content):
 
     pri = priority_alph.index(CommonItemRucksack(content)) + 1
 
     return pri
+### --- --- ###
 
+### --- Part two --- ###
+
+# find and calculate the badge priority
+def FindBadgePri(elf_group):
+    # elf group is a list of lenght 3
+    for letter in elf_group[0]:
+        if letter in elf_group[1] and letter in elf_group[2]:
+            return priority_alph.index(letter) + 1
+    #
+
+# group elf rucksacks
+def ElfGrouping(sacks):
+    # sacks is the data from input
+    groups = []
+    
+    for i in range(0, len(sacks), 3):
+        groups.append(sacks[i: i+3])
+    
+    return groups
+
+
+### --- --- ###
 # main
 def main():
 
+    # Part one
     sum_of_pri_common = 0
 
     for ruck in rucksacks:
         sum_of_pri_common += CalcPriority(ruck)
     
     print(f"Sum of priorities: {sum_of_pri_common}")
+
+    # Part two
+    sum_of_group_badge = 0
+    elf_groups = ElfGrouping(rucksacks)
+
+    for gr in elf_groups:
+        sum_of_group_badge += FindBadgePri(gr)
+    
+    print(f"Sum of group badges: {sum_of_group_badge}")
 
 # run program
 if __name__ == "__main__":
